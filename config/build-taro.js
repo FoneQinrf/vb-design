0c2aeff37c5869b1823c991bf5998611cf392538
Fone丶峰 <15921712019@163.com>
2023-01-31 12:49:45 +0800

    fix
    test

--- config/build-taro.js
+++ config/build-taro.js
@@ -11,21 +11,24 @@ if (fs.existsSync(resolve('packages/ui-taro/dist'))) {
 const fileList = [];
 console.log(resolve());
 console.log(resolve(__dirname));
-fs.copySync(resolve('packages/ui-h5/dist'), resolve('packages/ui-taro/dist'), {
-  filter: file => {
-    const arr = file.split(sep);
-    const flag = arr[arr.length - 2] === 'utils' ? true : !filterList.includes(arr[arr.length - 1]);
-    fileList.push(file);
-    return flag;
-  },
-});
+(async function () {
+  console.log(await glob(`${resolve('packages/ui-h5')}/**`));
+})();
+// fs.copySync(resolve('../packages/ui-h5/dist'), resolve('../packages/ui-taro/dist'), {
+//   filter: file => {
+//     const arr = file.split(sep);
+//     const flag = arr[arr.length - 2] === 'utils' ? true : !filterList.includes(arr[arr.length - 1]);
+//     fileList.push(file);
+//     return flag;
+//   },
+// });
 
-fileList.forEach(item => {
-  const arr = item.split(sep);
-  if (arr[arr.length - 1].indexOf('taro') !== -1) {
-    fs.removeSync(item);
-  }
-});
+// fileList.forEach(item => {
+//   const arr = item.split(sep);
+//   if (arr[arr.length - 1].indexOf('taro') !== -1) {
+//     fs.removeSync(item);
+//   }
+// });
 
 // (async function () {
 //   const files = await glob(`${resolve('packages/ui-taro/dist')}/**/**.js`);
